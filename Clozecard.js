4732c4424785f39353aec5efcadf9558b27b0670
Safiyya Sharif <SafiyyaSharif@lawn-128-61-12-57.lawn.gatech.edu>
2017-07-25 18:28:56 -0400

    initial cmmit

---
+++ Clozecard.js
@@ -0,0 +1,9 @@
+function ClozeCard(cloze, partial, fullText) {
+	this.cloze = cloze;
+	this.partial = partial;
+	this.fullText = fullText;
+}
+
+
+
+module.exports = ClozeCard;
---
+++ cloze-cards.js
@@ -0,0 +1,7 @@
+var inquirer = require ("inquirer");
+var ClozeCard = require ("./Clozecard.js");
+var ClozeCards = require ("./cloze.json");
+var fs = require ("fs");
+
+
+
